c4e4795c37aa97975916fc0d0b7cd0072d13cf1a
Ajay Sonere <ajaysonere786@gmail.com>
2022-10-29 10:56:24 +0530

    Front-End

--- Full Stack Web Development/01. Front-End Development/JAVASCRIPT/03. Switch Statement/app.js
+++ Full Stack Web Development/01. Front-End Development/JAVASCRIPT/03. Switch Statement/app.js
@@ -1,1 +1,13 @@
-alert("Hello papa");
+var x = 3;
+switch(x){
+    case 1: console.log("case 1 is executed");
+            break;
+    case 2: console.log("case 2 is executed");
+            break;
+    case 3: console.log("case 3 is executed");
+            break;
+    case 4: console.log("case 4 is executed");
+            break;
+    case 5: console.log("case 5 is executed");
+            break; 
+}
