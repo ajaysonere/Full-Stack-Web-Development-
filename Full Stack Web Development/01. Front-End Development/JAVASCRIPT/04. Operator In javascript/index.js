3c5015a1167b00aa58e0b85622231c6ab7a9749c
Ajay Sonere <ajaysonere786@gmail.com>
2022-10-29 12:07:46 +0530

    Front-End

---
+++ Full Stack Web Development/01. Front-End Development/JAVASCRIPT/04. Operator In javascript/index.js
@@ -0,0 +1,31 @@
+let x = 10;
+
+document.writeln(`<br> ${x+5}`);
+document.writeln(`<br> ${x-5}`);
+document.writeln(`<br> ${x*5}`);
+document.writeln(`<br> ${x/5}`);
+document.writeln(`<br> ${x%5}`);
+document.writeln(`<br> ${x**5}`);
+
+document.writeln("<br>" + x++);
+document.write(`<br> ${x}`);
+
+/* Data types in Javascript 
+   primitive Data types 
+1. Numbers
+2. strings
+3. booleans
+4. undefined
+5. null
+6. symbol
+*/
+const day = 8;
+console.log(day);
+console.log(typeof day);
+
+ var name = "Mr. pappu";
+console.log(name);
+console.log(typeof name);
+
+// concatination
+console.log(`${day} ${name}`);
