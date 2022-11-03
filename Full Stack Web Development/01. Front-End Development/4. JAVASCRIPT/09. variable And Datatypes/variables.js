fd9f67f43cb251abb8516fa5498a31d39e5e76a8
Ajay Sonere <ajaysonere786@gmail.com>
2022-11-03 22:34:09 +0530

    Front-End

---
+++ Full Stack Web Development/01. Front-End Development/4. JAVASCRIPT/09. variable And Datatypes/variables.js
@@ -0,0 +1,18 @@
+function multiplication(x,y){
+    if(x%3==0){
+        var z = 2*x*y;
+    }
+    else{
+        var w = 3*x*y;
+        if(w>0){
+            let cc = 10;
+            console.log(cc)
+        }
+    }
+    console.log(z);
+    console.log(w);
+    console.log(cc);
+}
+
+// console.log(z);
+multiplication(4,3);
