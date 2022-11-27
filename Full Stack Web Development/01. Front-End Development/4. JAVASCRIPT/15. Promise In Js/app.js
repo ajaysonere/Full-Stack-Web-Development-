8bd6bf1c98d513c0d67ac1618c6d27b6dcd26dd6
Ajay Sonere <ajaysonere786@gmail.com>
2022-11-27 16:09:03 +0530

    Front-End

---
+++ Full Stack Web Development/01. Front-End Development/4. JAVASCRIPT/15. Promise In Js/app.js
@@ -0,0 +1,17 @@
+/*
+
+1.  Register
+2.  Send Welcome email
+3.  Login
+4.  Get user data 
+5.  display User Data
+
+*/
+
+function register(a,b){
+    setTimeout((a,b)=>{
+        console.log(`Sum is ${a+b}`);
+    },1000,a,b)
+}
+
+register(10,20);
