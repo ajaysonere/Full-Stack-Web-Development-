20de3781b9e7cc441f01bc4133459f4dea3eceee
Ajay Sonere <ajaysonere786@gmail.com>
2022-11-27 21:24:33 +0530

    Front-End

---
+++ Full Stack Web Development/01. Front-End Development/4. JAVASCRIPT/12. DOM Manipulation/app.js
@@ -0,0 +1,71 @@
+console.log('DOM manipulation');
+
+
+// getElementById 
+
+
+// const heading = document.getElementById('heading');
+// console.log(heading);
+
+// // getElementByTagName
+
+// const head = document.getElementsByTagName('h3');
+// console.log(head);
+
+// // getElementByClassName
+
+// const classhead = document.getElementsByClassName('para');
+
+// console.log(classhead);
+
+// // querySelector
+
+// const querypara = document.querySelector('p');
+// console.log(querypara);
+
+// // querySelectorAll retrun the nodeCollection 
+
+
+// const child = document.querySelector('.heading');
+// console.log(child);
+// const parent = child.parentNode;
+// console.log(parent);
+
+// const heading = document.querySelector('.heading');
+
+// console.log(heading.nextElementSibling); 
+
+// const heading  = document.querySelector('.heading');
+// heading.innerHTML = " I Miss You Papa";
+
+// heading.style.color = 'red';
+
+// heading.style.fontSize = '22px';
+// heading.style.fontFamily = "Arial";
+
+// const heading = document.createElement('h2');
+// heading.innerHTML = "This is Heading ";
+
+// console.log(heading);
+
+// const parent = document.querySelector('.parent');
+
+// parent.appendChild(heading);
+// console.log(heading);
+
+// const button = document.querySelector('#btn');
+// // const father = document.querySelector('#father)'
+// const father = document.createElement('h3');
+// father.innerHTML = "Mr.Pappu Sonere";
+
+// const parent = document.querySelector('body');
+// parent.appendChild(father);
+
+
+
+// button.addEventListener('click',()=>{
+//     father.style.color = "blue";
+//     father.style.fontSize = "18px";
+//     father.style.fontFamily = "sans-serif";
+//     console.log("Button Clicked");
+// })
--- Full Stack Web Development/01. Front-End Development/4. JAVASCRIPT/15. Promise In Js/app.js
+++ Full Stack Web Development/01. Front-End Development/4. JAVASCRIPT/15. Promise In Js/app.js
@@ -8,10 +8,102 @@
 
 */
 
-function register(a,b){
-    setTimeout((a,b)=>{
-        console.log(`Sum is ${a+b}`);
-    },1000,a,b)
+// function register(a,b){
+//     setTimeout((a,b)=>{
+//         console.log(`Sum is ${a+b}`);
+//     },1000,a,b)
+// }
+
+// register(10,20);
+
+
+function register(){
+   return new Promise((resolve,reject)=>{
+    setTimeout(() => {
+        console.log("Register End");
+        resolve();
+     },2000);
+   });
 }
 
-register(10,20);
+function sendEmail(){
+  return new Promise ((resolve,reject)=>{
+    setTimeout(() => {
+       return reject('send Email');
+        console.log("Email End");
+      },3000);
+  });
+}
+
+function login(){
+  return new Promise((resolve,reject)=>{
+    setTimeout(() => {
+        console.log("Login End");
+        resolve();
+      },6000);
+  });
+}
+
+function getUserData(){
+  return new Promise((resolve,reject)=>{
+    setTimeout(() => {
+        console.log("GetUserData End");
+        resolve();
+      },5000);
+  });
+}
+
+function displayUserData(){
+  return new Promise((resolve,reject)=>{
+    setTimeout(() => {
+        console.log("Display user data End");
+        resolve();
+      },1000);
+  })
+}
+
+console.log("Promises In Progress :");
+
+
+// Call Back Hell 
+// register(()=>{
+//   sendEmail( ()=>{
+//     login(()=>{
+//       getUserData(()=>{
+//         displayUserData();
+//       });
+//     });
+//   });
+// });
+
+
+// register()
+// .then(sendEmail)
+// .then(login)
+// .then(getUserData)
+// .then(displayUserData)
+// .catch(()=>{
+//     console.log("Error While Processing ... ");
+// });
+
+
+async function authenticate(){
+   
+   try{
+    await register();
+    await sendEmail();
+    await login();
+    await getUserData();
+    await displayUserData();
+   }
+   catch(err){
+       console.log("Error Has Occured in "+err);
+   }
+   throw new Error();
+}
+
+authenticate().then(()=>{
+    console.log("All Set");
+}).catch((err)=>{
+    console.log("Error has Occured in Authenticate");
+});
