5b81cc6954080eef266b2606cccc23c5444dbbc1
Ajay Sonere <ajaysonere786@gmail.com>
2022-11-27 14:18:44 +0530

    Front-End

---
+++ Full Stack Web Development/01. Front-End Development/4. JAVASCRIPT/17. asynchronous and synchronos/Asynchronous.js
@@ -0,0 +1,18 @@
+// synchronous JS
+
+console.log('Hello');
+
+console.log("Javascript");
+
+
+// asynchronous JS
+
+console.log("Hello");
+
+setTimeout(()=>{
+    console.log('This is SetTimeout');
+},5000);
+console.log("Javascript");
+
+
+
--- Full Stack Web Development/01. Front-End Development/4. JAVASCRIPT/17. asynchronous and synchronos/callbackhell.js
+++ Full Stack Web Development/01. Front-End Development/4. JAVASCRIPT/17. asynchronous and synchronos/callbackhell.js
@@ -1,11 +1,92 @@
-// callback Function 
-function loadScript(src){
-    let script = document.createElement("script");
-    script.src = src;
-    script.onload = ()=>{
-      console.log("Loaded script with SRC ");
-    }
-    document.body.appendChild(script);
-}
+// CallBackHell In JavaScript 
 
-loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js");
+// secnario
+
+/*
+
+
+1.  Register
+2.  Send Welcome email
+3.  Login
+4.  Get user data 
+5.  display User Data
+
+
+*/
+
+// function register(callback){
+//    setTimeout(() => {
+//        console.log("Register End");
+//        callback();
+//     },2000);
+// }
+
+// function sendEmail(callback){
+//   setTimeout(() => {
+//     console.log("Email End");
+//     callback();
+//   },3000);
+// }
+
+// function login(callback){
+//   setTimeout(() => {
+//     console.log("Login End");
+//     callback();
+//   },6000);
+// }
+
+// function getUserData(callback){
+//   setTimeout(() => {
+//     console.log("GetUserData End");
+//     callback();
+//   },4000);
+// }
+
+// function displayUserData(){
+//   setTimeout(() => {
+//     console.log("Display user data End");
+//   },1000);
+// }
+
+
+
+// // Call Back Hell 
+// register(()=>{
+//   sendEmail( ()=>{
+//     login(()=>{
+//       getUserData(()=>{
+//         displayUserData();
+//       });
+//     });
+//   });
+// });
+
+// console.log("Othe Application Works");
+
+
+console.log("Callback In Progess...")
+
+setTimeout(()=>{
+  console.log("Fetching Data Start :");
+  let ids = [10,20,30,40,50];
+  console.log(ids);
+  console.log("Fetching Data End");
+  setTimeout((ids)=>{
+     console.log("Fetching Ideas Start :");
+     let id = {
+              id:2,
+              fastname:"Ajay",
+              lastname:"sonere",
+              age:20
+            }
+    console.log(id);
+    setTimeout((age)=>{
+      console.log(`Hello I am checking ,Your Eligibility  ${age}`);
+      if(age>18){
+        console.log("Your Are Eligibal");
+      }else{
+        console.log("Your are not Eligibal");
+      }
+    },8000,id.age)
+  },5000,ids[2])
+},3000)
