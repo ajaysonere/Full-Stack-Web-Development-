e39a1aba4fab7c073d63cea9a1a2aec9d3a7b3cc
Ajay Sonere <ajaysonere786@gmail.com>
2022-11-28 15:35:27 +0530

    Front-End

---
+++ Full Stack Web Development/02. Back-End Development/01. Node JS/YT Node/module.js
@@ -0,0 +1,29 @@
+// const color = require('cli-color');
+
+// console.log(color.blue('Hello Node JS'));
+
+// console.log(color.green('Hello Node JS'));
+
+// console.log(color.red('Hello Node JS'));
+
+// console.log(color.redBright('Hello Node JS'));
+
+const path = require('path')
+
+// dirname
+// console.log('Folder Name ', path.dirname(__filename));
+
+// console.log('File Name ', path.basename(__filename));
+
+// console.log(__filename);
+
+// console.log('Extension Name ', path.extname(__filename));
+
+// parse 
+// console.log('parse : ', path.parse(__filename));
+
+// join 
+
+// console.log("Join :",path.join(__dirname,'node js','module.js'));
+
+// File System 
