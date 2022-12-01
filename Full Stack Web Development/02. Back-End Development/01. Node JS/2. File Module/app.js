a43f5dd5721312fef11c6260939db0aa6ce30355
Ajay Sonere <ajaysonere786@gmail.com>
2022-12-01 16:04:32 +0530

    Back -End

--- Full Stack Web Development/02. Back-End Development/01. Node JS/2. File Module/app.js
+++ Full Stack Web Development/02. Back-End Development/01. Node JS/2. File Module/app.js
@@ -1,1 +1,47 @@
 const http = require('http');
+const fs = require('fs');
+const myModule = require('./src/mymodule');
+
+let gdata = "A";
+const PORT = 7800;
+const hostName = "localhost";
+
+const server =  http.createServer((req,res)=>{
+    fs.writeFile('instruction.txt',"B. It is the First Instruction",()=>{
+        console.log('Write Operation Is Completed \n');
+    });
+
+    fs.readFile('instruction.txt',(error,data)=>{
+        
+        if(error){
+            console.log("Something Is Wrong...");
+        }else{
+           gdata = data.toString();
+           console.log('read Successfully');
+        }
+    });
+
+    res.write(`Instruction is : ${gdata} \n`);
+    let sum = myModule.addition(10,20);
+
+    let substraction = myModule.substraction(20,10);
+
+    let multiplication = myModule.multiplication(10,20);
+
+    let division = myModule.division(50,5);
+
+    res.write(`Sum is :${sum} \n`);
+
+    res.write(`Substraction is :${substraction} \n`);
+
+    res.write(`Multiplication is : ${multiplication} \n`);
+
+    res.write(`Divison of ther Number is : ${division} \n`);
+    console.log("After the Write File");
+    res.write('It is Write Response \n');
+    res.end();
+});
+
+server.listen(PORT,hostName,()=>{
+    console.log(`Server is Running at : http://${hostName}:${PORT}`);
+});
---
+++ Full Stack Web Development/02. Back-End Development/01. Node JS/2. File Module/src/mymodule.js
@@ -0,0 +1,15 @@
+exports.addition = (a,b)=>{
+    return a+b;
+};
+
+exports.substraction = (a,b)=>{
+    return a-b;
+};
+
+exports.multiplication = (a,b)=>{
+    return a*b;
+};
+
+exports.division = (a,b)=>{
+    return a/b;
+};
