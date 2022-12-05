a3902b1311306400b2b4128d67c6c548421964ba
Ajay Sonere <ajaysonere786@gmail.com>
2022-12-05 15:36:10 +0530

    Front-End

---
+++ Full Stack Web Development/01. Front-End Development/5. HTML CSS AND JS PROJECT/06. Count Number/app.js
@@ -0,0 +1,39 @@
+// ser initialcount
+let count =0;
+
+// select value and btn
+let value = document.querySelector('#value');
+let btns = document.querySelectorAll('.btn');
+
+btns.forEach((btn)=>{
+//    console.log(btn)
+
+    //   For value of value in span 
+    btn.addEventListener('click',(e)=>{
+       const clickbtn = e.target.classList;
+       if(clickbtn.contains('decrease')){
+        count--;
+       }
+      else if(clickbtn.contains('increase')){
+        count++;
+       }
+       else{
+        count =0;
+       }
+
+        //  Text Color 
+       if(count>0){
+         value.style.color = 'green';
+       }
+       else if(count<0){
+        value.style.color = 'red';
+       }
+       else{
+        value.style.color = 'black';
+       }
+
+       value.textContent = count;
+
+
+    })
+})
