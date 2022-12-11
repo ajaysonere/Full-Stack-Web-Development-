5e91470d63b84f9946ae01129e71f0ead81f95f6
Ajay Sonere <ajaysonere786@gmail.com>
2022-12-11 18:33:47 +0530

    Front-End

--- Full Stack Web Development/01. Front-End Development/5. HTML CSS AND JS PROJECT/07. Clock Project/app.js
+++ Full Stack Web Development/01. Front-End Development/5. HTML CSS AND JS PROJECT/07. Clock Project/app.js
@@ -1,1 +1,22 @@
-console.log('Status:OK');
+let hourEl = document.querySelector('.hour');
+let minuteEl = document.querySelector('.minute')
+let secondeEl = document.querySelector('.seconde');
+
+function updateClock(){
+  let currentDate = new Date();
+  setTimeout(updateClock,1000);
+  const hour = currentDate.getHours();
+  const minute = currentDate.getMinutes();
+  const seconde = currentDate.getSeconds();
+  console.log(hour,seconde,minute);
+  
+  const hourDeg = (hour / 12) *360;
+  hourEl.style.transform = `rotate(${hourDeg}deg)`;
+  
+  const minuteDeg = (minute / 60) *360;
+  minuteEl.style.transform = `rotate(${minuteDeg}deg)`;
+  
+  const secondeDeg = (seconde / 60) *360;
+  secondeEl.style.transform = `rotate(${secondeDeg}deg)`;
+}
+updateClock();
