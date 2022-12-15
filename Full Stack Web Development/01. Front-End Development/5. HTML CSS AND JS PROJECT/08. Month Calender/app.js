83ba0e8c191c964997dc3482ca40961e2a1a1e27
Ajay Sonere <ajaysonere786@gmail.com>
2022-12-15 21:59:12 +0530

    Front-End

--- Full Stack Web Development/01. Front-End Development/5. HTML CSS AND JS PROJECT/08. Month Calender/app.js
+++ Full Stack Web Development/01. Front-End Development/5. HTML CSS AND JS PROJECT/08. Month Calender/app.js
@@ -1,6 +1,12 @@
 const monthEl = document.querySelector('.date h1');
 const datesEl = document.querySelector('.date p');
+const days = document.querySelector('.days')
 const monthsIndex = new Date().getMonth();
+
+
+const lastDay = new Date(new Date().getFullYear(),monthsIndex+1,0).getDate();
+
+const firstDay = new Date(new Date().getFullYear(),monthsIndex,1).getDay()-1;
 
 const months = [
     "January",
@@ -17,4 +23,21 @@ const months = [
     "december"
 ]
 monthEl.innerText = months[monthsIndex];
-datesEl.innerText = new Date().toDateString();bhjhbn
+
+datesEl.innerText = new Date().toDateString();
+let day = "";
+
+
+for(let i=firstDay; i>0 ;i--){
+   day += `<div class = "empty"> </div>`;
+}
+
+for(let i=1;i<=lastDay;i++){
+    if(i === new Date().getDate()){
+        day += `<div class = "today">${i}</div>`
+    }else{
+        day += `<div class = "dates"> ${i} </div>`;
+    }
+}
+
+days.innerHTML = day;
