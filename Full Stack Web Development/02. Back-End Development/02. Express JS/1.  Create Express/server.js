342a28ec055f0794a4c11e8282591efa04388e95
Ajay Sonere <ajaysonere786@gmail.com>
2022-12-19 16:51:45 +0530

    Back-End

--- Full Stack Web Development/02. Back-End Development/02. Express JS/1.  Create Express/server.js
+++ Full Stack Web Development/02. Back-End Development/02. Express JS/1.  Create Express/server.js
@@ -12,18 +12,25 @@ app.get("/sum/:a/:b",(req,res)=>{
     let a =parseInt(req.params.a);
     let b =parseInt(req.params.b);
     let c =a+b;
-    res.send(`sum is : ${c}`);
+    res.json(`{sum is : ${c}}`);
 });
 
 app.get("/factorial/:n",(req,res)=>{
-    let fac = 1;
-    let n = parseInt(req.params.n);
-    for(let i=1;i<=n;i++){
-        fac *= i;
-    }
-    res.send(`Factorial is : ${fac}`);
+     try{
+        let n = parseInt(req.params.n);
+        if(n>0){
+            let fac = 1;
+            for(let i=1;i<=n;i++){
+                fac *= i;
+            }
+            res.status(201).json({Factorial :fac});  
+        }else{
+            res.status(400).json({message:"Only Positive Number are allowed "}); 
+        }
+     } catch(error){
+        res.status(500).json({message:'something went wrong '});
+     }
 })
-
 
 app.listen(process.env.PORT,()=>{
 console.log(`server is running at http://`+process.env.HOSTNAME +":"+ process.env.PORT);
