a187f586c00e606d1127261e0dad224366b6b6a4
Ajay Sonere <ajaysonere786@gmail.com>
2022-12-21 14:01:03 +0530

    Back-End

--- Full Stack Web Development/02. Back-End Development/02. Express JS/1.  Create Express/post.js
+++ Full Stack Web Development/02. Back-End Development/02. Express JS/1.  Create Express/post.js
@@ -1,22 +1,32 @@
 import http from 'http';
 import url from 'url';
 import fs from 'fs';
+import express from 'express';
+import StatusCodes from 'http-status-codes';
+
+const app = express();
 // const server = http.createServer((req,res)=>{
 //    let q = url.parse(req.url,true).query;
 //    let txt = q.year + " " + q.month;
 //    res.end(txt);
 // })
-let data = 'HelloWorld';
-const server = http.createServer((req,res)=>{
-    fs.writeFileSync('demo.html','<p>This is Para</p>')
+// let data = 'HelloWorld';
+// const server = http.createServer((req,res)=>{
+//     fs.writeFileSync('demo.html','<p>This is Para</p>')
 
-    const read = fs.readFileSync('demo.html',()=>{
-        console.log('File has read succesfully');
-    });
-    res.write('Home Page '+ read);
-    res.end();
-});
+//     const read = fs.readFileSync('demo.html',()=>{
+//         console.log('File has read succesfully');
+//     });
+//     res.write('Home Page '+ read);
+//     res.end();
+// });
 
-server.listen(7800,'localhost',()=>{
+app.get('/',(req,res)=>{
+    res.status(StatusCodes.OK).send('This is Home Page ');
+})
+
+
+
+app.listen(7800,'localhost',()=>{
     console.log(`Server is running on http://localhost:7800`);
 })
