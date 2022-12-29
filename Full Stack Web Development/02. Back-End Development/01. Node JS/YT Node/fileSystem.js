797c5b4ddf47a0a2e3edd753a425dcf319434d72
Ajay Sonere <ajaysonere786@gmail.com>
2022-12-29 15:47:28 +0530

    Back-End

--- Full Stack Web Development/02. Back-End Development/01. Node JS/YT Node/fileSystem.js
+++ Full Stack Web Development/02. Back-End Development/01. Node JS/YT Node/fileSystem.js
@@ -1,1 +1,39 @@
 import fs from "fs";
+import {join} from "path";
+ 
+// Create a folder
+fs.mkdir(join('.','/src'),(error)=>{
+     if(error){
+        console.log(`Something went wrong `);
+     }else{
+        console.log(`Folder created`);
+     }
+});
+
+
+// Create a file
+fs.writeFile(join('.','src','text.txt'),'hello World ',(err)=>{
+    if(err){
+        console.log(`Error has occured`);
+    }else{
+        fs.appendFile(join('.','src','text.txt'),' It is node course',(err)=>{
+            if(err){
+                console.log(`Error has occured while appending data`);
+            }else{
+                console.log(`Data Appended succesfully`);
+            }
+        })
+        console.log('File Created');
+    }
+})
+
+
+// read file
+
+fs.readFile(join('.','src','text.txt'),(err,data)=>{
+    if(err){
+        console.log('Error has occured while reading the file');
+    }else{
+        console.log(`File data :- ${data}`)
+    }
+})
