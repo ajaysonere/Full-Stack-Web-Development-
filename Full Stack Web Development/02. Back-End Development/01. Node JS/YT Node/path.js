c75780c5269210c8b560bd197d79f398936514f1
Ajay Sonere <ajaysonere786@gmail.com>
2022-12-29 12:34:53 +0530

    Back-End

--- Full Stack Web Development/02. Back-End Development/01. Node JS/YT Node/path.js
+++ Full Stack Web Development/02. Back-End Development/01. Node JS/YT Node/path.js
@@ -1,1 +1,20 @@
-import path from 'path';
+import { log } from 'console';
+import { basename, dirname, join, parse,} from 'path';
+import {fileURLToPath} from 'url';
+
+// console.log('folder name', path.dirname(__filename));
+
+const __filename = fileURLToPath(import.meta.url);
+// console.log(__filename);
+
+const __dirname = dirname(__filename);
+// console.log(__dirname);
+
+const __basename  = basename(__filename);
+console.log(__basename);
+
+const __parse = parse(__filename);
+console.log(__parse);
+
+const __join = join(__dirname, 'home','app.js');
+console.log(__join);
