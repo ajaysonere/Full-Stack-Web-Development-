5975fb6e0783077eb5bf89a188ea116a7b750d49
Ajay Sonere <ajaysonere786@gmail.com>
2022-12-29 10:47:37 +0530

    Back-End

--- Full Stack Web Development/02. Back-End Development/01. Node JS/YT Node/app.js
+++ Full Stack Web Development/02. Back-End Development/01. Node JS/YT Node/app.js
@@ -1,3 +1,15 @@
-function square(n){
-    return n **2; 
-}
+// import http from 'http';
+import path from 'path'   
+// const http = require('http');
+
+
+
+const server = http.createServer((req,res)=>{
+    res.write('Hello World');
+    res.write('folder path'+path.dirname(__filename));
+    res.end();
+});
+server.listen(7800,()=>{
+    // console.log('Folder name:', path.dirname(__filename));
+    console.log(`server is running at : http://localhost:7800`);
+})
