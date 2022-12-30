5baea945f4f9050af3c1c8289758f5433d797cdc
Ajay Sonere <ajaysonere786@gmail.com>
2022-12-30 22:26:05 +0530

    Back-End

---
+++ Full Stack Web Development/02. Back-End Development/02. Express JS/YT EXPRESS/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import { fileURLToPath } from 'url'
+import  { basename, dirname, extname, join } from 'path';
+
+const PORT = process.env.PORT || 7800
+const app = express();
+const filePath = fileURLToPath(import.meta.url);
+
+
+app.get('/',(req,res)=>{
+    res.sendFile(dirname(filePath)+'/src/index.html');
+})
+
+app.get('/about',(req,res)=>{
+    res.sendFile(dirname(filePath)+'/src/about.html');
+})
+
+app.get('/contect',(req,res)=>{
+    res.sendFile(dirname(filePath)+'/src/contect.html');
+});
+
+app.get('/service',(req,res)=>{
+    res.sendFile(dirname(filePath)+'/src/service.html');
+})
+
+app.listen(7800,()=>{
+    console.log(`server is running at http://localhost:${PORT}`);
+})
