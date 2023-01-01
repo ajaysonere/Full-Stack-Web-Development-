11e2aa6afad745a1478d95bff069c11f60ac301b
Ajay Sonere <ajaysonere786@gmail.com>
2023-01-01 12:14:11 +0530

    Back-End

---
+++ Full Stack Web Development/01. Front-End Development/5. HTML CSS AND JS PROJECT/09. Form Validation/app.js
@@ -0,0 +1,13 @@
+const form = document.querySelector('#form');
+const username = document.querySelector('#username');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const cpassword = document.querySelector('#cpassword');
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+
+    checkInputs();
+});
+
+ 
--- Full Stack Web Development/02. Back-End Development/03. MongoDB/02. CRUD Operation/src/router/employeeRouter.js
+++ Full Stack Web Development/02. Back-End Development/03. MongoDB/02. CRUD Operation/src/router/employeeRouter.js
@@ -6,13 +6,9 @@ const employeeRouter = express.Router();
 employeeRouter.post('/employees',saveEmployee);
 employeeRouter.get('/employees',fetchAllEmployee);
 employeeRouter.get('/employees/:id',fetchEmployeeById);
-
 employeeRouter.get('/employees/name/:name',fetchEmployeeByName);
-
 employeeRouter.get('/employees/phone/:phone',fetchEmployeeByPhone);
-
 employeeRouter.delete('/employees/:id',deleteEmployee);
-
 employeeRouter.put('/employees/:id',updateEmployee); 
 
 export default employeeRouter;
