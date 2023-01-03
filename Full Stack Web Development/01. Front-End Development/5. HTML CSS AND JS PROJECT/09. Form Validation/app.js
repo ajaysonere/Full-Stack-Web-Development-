ee06f32a04e074b07e1d641ecaff0ba6786f462a
Ajay Sonere <ajaysonere786@gmail.com>
2023-01-03 16:41:32 +0530

    Back-End

--- Full Stack Web Development/01. Front-End Development/5. HTML CSS AND JS PROJECT/09. Form Validation/app.js
+++ Full Stack Web Development/01. Front-End Development/5. HTML CSS AND JS PROJECT/09. Form Validation/app.js
@@ -9,5 +9,27 @@ form.addEventListener('submit',(e)=>{
 
     checkInputs();
 });
+function checkInputs(){
+   const usernameValue =  username.value.trim();
+   const emailValue =  email.value.trim();
+   const passwordValue =   password.value.trim();
+   const cpasswordValue =  cpassword.value.trim();
 
- 
+   if(usernameValue === '' ){  
+        setErrorFor(username,'UserName cannot be blank');
+   }else{
+    setSuccessFor(username)
+   }
+}
+
+function setErrorFor(username,message){
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+    formControl.className = 'form-control error';
+}
+
+function setSuccessFor(username){
+     const formControl = input.parentElement;
+     formControl.className = "form-control success"
+}
