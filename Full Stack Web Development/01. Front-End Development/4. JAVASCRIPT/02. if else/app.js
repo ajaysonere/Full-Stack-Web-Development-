167df898ddc66d18b5db7c7d286a975810c4aad3
Ajay Sonere <ajaysonere786@gmail.com>
2023-01-10 23:02:18 +0530

    Front-End

---
+++ Full Stack Web Development/01. Front-End Development/4. JAVASCRIPT/02. if else/app.js
@@ -0,0 +1,12 @@
+// In this lecture we are learning trythy and falsey 
+
+const value = 4;
+
+// Truthy Value - 1 
+// falsey Value - 0 , "" ,null ,undefined ,
+
+if(value){
+    console.log('This is Value is Truthy ');
+}else{
+    console.log('This is Falsey Value ');
+}
