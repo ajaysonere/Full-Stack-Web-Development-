b8171461e87bd0aebdb36bad27e9c34376502dde
Ajay Sonere <ajaysonere786@gmail.com>
2023-01-11 23:33:45 +0530

    Front-End

--- Full Stack Web Development/01. Front-End Development/4. JAVASCRIPT/12. DOM Manipulation/index.js
+++ Full Stack Web Development/01. Front-End Development/4. JAVASCRIPT/12. DOM Manipulation/index.js
@@ -13,6 +13,46 @@
 // console.log(header);
 // console.log(header[0]);
 
+// const collectionItems = document.getElementsByClassName('item');
+
+// const nodeItems = document.querySelector("#todo-list");
+
+// console.log(collectionItems , nodeItems);
+// const nodeChilds  = document.querySelector("#todo-list");
+// console.log(nodeChilds.childNodes); 
+
+// const createElement = document.querySelector('.item');
+// console.log(createElement);
+
+// const todoList = document.getElementById('todo-list');
 
 
-  
+// const newItem = document.createElement("li");
+// newItem.classList.add('item');
+// newItem.innerText = 'ITEM 4';
+
+// nodeItems.appendChild(newItem);
+// console.log(nodeItems);
+
+// const todoNr = document.getElementsByClassName('todo-Nr')[0];
+// todoNr.innerText = collectionItems.length;
+
+
+const nodeList =  document.querySelector("#todo-list");
+const listNumber = document.querySelectorAll('.item')
+const item = nodeList.children;
+
+const newItem = document.createElement('li');
+newItem.classList.add('item');
+newItem.innerHTML = "ITEM 3";
+
+const newItem4 = document.createElement('li');
+newItem.classList.add('item');
+newItem.innerHTML = "ITEM 4";
+
+const liNumber= document.querySelectorAll('.todoNr')[0];
+
+nodeList.appendChild(newItem);
+nodeList.appendChild(newItem4);
+
+liNumber.innerText = item.length;
