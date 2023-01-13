51b504f19c832f2d9af43a1d64294da961d6b1d7
Ajay Sonere <ajaysonere786@gmail.com>
2023-01-13 23:24:22 +0530

    Front-End

--- Full Stack Web Development/01. Front-End Development/4. JAVASCRIPT/12. DOM Manipulation/event.js
+++ Full Stack Web Development/01. Front-End Development/4. JAVASCRIPT/12. DOM Manipulation/event.js
@@ -1,13 +1,20 @@
 const btn = document.querySelector('#submit');
 const todoList = document.querySelector('#todoList');
 const todoNr = document.querySelector('.todoNr b');
+const mainTitle = document.querySelector('.mainTitle');
 const items = todoList.children;
 
+// btn.addEventListener('click' , ()=>{
+//      const newItem = document.createElement('li');
+//      newItem.classList.add('Item');
+//      newItem.innerText = `Item ${items.length+1}`;
+//      todoNr.innerText = `${items.length+1}`;
+//      todoList.appendChild(newItem);
+//      console.log('Element is created ');
+// }); 
+
 btn.addEventListener('click' , ()=>{
-     const newItem = document.createElement('li');
-     newItem.classList.add('Item');
-     newItem.innerText = `Item ${items.length+1}`;
-     todoNr.innerText = `${items.length+1}`;
-     todoList.appendChild(newItem);
-     console.log('Element is created ');
-}); 
+    // mainTitle.style.color = "red";
+    // mainTitle.style.fontSize = "50px";
+    mainTitle.classList.toggle("display");
+});
