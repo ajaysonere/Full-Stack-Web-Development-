436eeb28aa028b4e3379e38952bc0beb61d77b8b
Ajay Sonere <ajaysonere786@gmail.com>
2023-01-14 14:26:28 +0530

    Front-End

--- Full Stack Web Development/01. Front-End Development/4. JAVASCRIPT/12. DOM Manipulation/event.js
+++ Full Stack Web Development/01. Front-End Development/4. JAVASCRIPT/12. DOM Manipulation/event.js
@@ -4,17 +4,35 @@ const todoNr = document.querySelector('.todoNr b');
 const mainTitle = document.querySelector('.mainTitle');
 const items = todoList.children;
 
-// btn.addEventListener('click' , ()=>{
-//      const newItem = document.createElement('li');
-//      newItem.classList.add('Item');
-//      newItem.innerText = `Item ${items.length+1}`;
-//      todoNr.innerText = `${items.length+1}`;
-//      todoList.appendChild(newItem);
-//      console.log('Element is created ');
-// }); 
-
 btn.addEventListener('click' , ()=>{
-    // mainTitle.style.color = "red";
-    // mainTitle.style.fontSize = "50px";
-    mainTitle.classList.toggle("display");
-});
+     const newItem = document.createElement('li');
+     newItem.classList.add('Item');
+     newItem.innerText = `Item ${items.length+1}`;
+     todoNr.innerText = `${items.length+1}`;
+     todoList.appendChild(newItem);
+     console.log('Element is created ');
+     newItem.addEventListener("click",(event)=>{
+         event.stopPropagation();
+         event.target.remove();
+         todoNr.innerText = `${items.length}`;
+         console.log("ITEM DELETED ");
+     })
+}); 
+
+todoList.addEventListener("click" , ()=>{
+    // It was running because of the event babling
+    console.log("UL EXCUTED ");
+    todoList.classList.add("fade");
+})
+
+// for(item of items){
+//     item.addEventListener("click",()=>{
+//         console.log(`ITEM DELETED `);
+//     }); 
+// }
+
+// btn.addEventListener('click' , ()=>{
+//     // mainTitle.style.color = "red";
+//     // mainTitle.style.fontSize = "50px";
+//     mainTitle.classList.toggle("display");
+// });
---
+++ Full Stack Web Development/01. Front-End Development/4. JAVASCRIPT/12. DOM Manipulation/sessionAndLocalStorage.js
@@ -0,0 +1,7 @@
+// Local Storage 
+
+// localStorage.setItem("todo" , "car washing ");
+
+// // Session Storage 
+
+// sessionStorage.setItem("study", "This is study session ");
