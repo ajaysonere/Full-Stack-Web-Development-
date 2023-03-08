93d34a1d0b3e006f87e76e60b60611e40afddeb5
Ajay Sonere <ajaysonere786@gmail.com>
2023-03-08 22:17:10 +0530

    revision

--- Full Stack Web Development/02. Back-End Development/01. Node JS/1. HTTP Module/app.js
+++ Full Stack Web Development/02. Back-End Development/01. Node JS/1. HTTP Module/app.js
@@ -4,10 +4,11 @@ const PORT = 7800;
 const server = http.createServer((req,res)=>{
     let add = numberModule.sum(2,7);
     let mul = numberModule.product(2,7);
-    res.write(` The Sum is ${add} and The Product is ${mul}`);
+    res.write(` The Sum is ${add} \n The Product is ${mul}`);
     res.end(' \n Response End');
 });
 
 server.listen(PORT,()=>{
-    console.log("Server is Runnig at :" + PORT);
+    // console.log("Server is Runnig at :" + PORT);
+    console.log(`localHost is runing On http://localhost:${PORT}`);
 });
